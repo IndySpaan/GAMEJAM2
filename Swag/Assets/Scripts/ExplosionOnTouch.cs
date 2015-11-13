3b041f95eed76a268f124f68bbcf1b8bf920fca4
frankhartman <f.hartman@student.fontys.nl>
2015-11-13 10:10:15 +0100

    Main game update

--- Swag/Assets/Scripts/ExplosionOnTouch.cs
+++ Swag/Assets/Scripts/ExplosionOnTouch.cs
@@ -2,25 +2,27 @@
 using System.Collections;
 
 public class ExplosionOnTouch : MonoBehaviour {
-    public float PushPower = 1f;
+    public float PushPower = 10f;
 
 
 
     void OnCollisionEnter(Collision hit)
+        
     {
         GameObject player = hit.gameObject;
         // If it is not a player, exit the method
-        if (!player.tag.Equals("Player"))
+        if (!player.tag.Equals("Player") || !player.tag.Equals("Ball"))
             return;
-
+        Debug.Log("Get body");
         // Get the rigidbody
         Rigidbody body = player.GetComponent<Rigidbody>();
-        if (body == null || body.isKinematic)
-            return;
+        //if (body == null)
+        //    return;
 
         // Give the force a direction
-        Vector3 pushDir = new Vector3(hit.transform.position.x - this.transform.position.x, 0, hit.transform.position.y - this.transform.position.y) * 10 ;
+        Debug.Log("Push the ball");
+        Vector3 pushDir = (hit.transform.position - this.transform.position) * 10 + new Vector3(0, 5, 0);
         body.AddForce(pushDir * PushPower);
-       
+
     }
 }
--- Swag/Assets/Scripts/ballInstantiate.cs
+++ Swag/Assets/Scripts/ballInstantiate.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ballInstantiate : MonoBehaviour {
+public class ballInstantiate : MonoBehaviour
+{
 
     private GameObject ballPrefab;
     private GameObject InstantiatedBall;
@@ -10,55 +11,48 @@ public class ballInstantiate : MonoBehaviour {
 
     private Vector3 explosionPosition;
     private float randomCorner;
-    void Start ()
+    void Start()
     {
         //Instantiating an amount of balls equal to numberOfBalls.
         //Adding a random force to the instantiated ball.
         ballPrefab = (GameObject)Resources.Load("Prefabs/ball");
+        GameObject parent = new GameObject();
+        parent.name = "Ballen";
 
-        for(int i = 0; i < numberOfBalls; i++)
+        for (int i = 0; i < numberOfBalls; i++)
         {
-            InstantiatedBall = (GameObject)Instantiate(ballPrefab, new Vector3(Random.Range(-10, 10), Random.Range(10, 15), Random.Range(-10, 10)), Quaternion.identity);
+            InstantiatedBall = (GameObject)Instantiate(ballPrefab, new Vector3(0, this.transform.position.y - 1, 0), Quaternion.identity);
             InstantiatedBall.GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-20, 20), -4, Random.Range(-20, 20));
+            InstantiatedBall.transform.SetParent(parent.transform);
         }
-	}
+    }
 
-    void Update()
+    void FixedUpdate()
     {
-        if (Random.Range(0f, 1f) > 0.98f)
+        explosions();
+
+    }
+
+    void explosions()
+    {
         {
-            randomCorner = Random.Range(0f, 1f);
+            if (Random.Range(0f, 2f) > 1.99f)
+            {
+               
+                    explosionPosition = new Vector3(0, 1, 0);
+                
 
-            if (randomCorner > 0.8f)
-            {
-                explosionPosition = new Vector3(-10.5f, 1, -10.5f);
-            }
-            else if (randomCorner > 0.6f)
-            {
-                explosionPosition = new Vector3(10.5f, 1, -10.5f);
-            }
-            else if (randomCorner > 0.4f)
-            {
-                explosionPosition = new Vector3(-10.5f, 1, 10.5f);
-            }
-            else if(randomCorner > 0.2f)
-            {
-                explosionPosition = new Vector3(10.5f, 1, 10.5f);
-            }
-            else
-            {
-                explosionPosition = new Vector3(0, 1, 0);
-            }
+                Collider[] colliders = Physics.OverlapSphere(explosionPosition, 10);
+                foreach (Collider hit in colliders)
+                {
+                    Rigidbody rb = hit.GetComponent<Rigidbody>();
 
-            Collider[] colliders = Physics.OverlapSphere(explosionPosition, 10);
-            foreach (Collider hit in colliders)
-            {
-                Rigidbody rb = hit.GetComponent<Rigidbody>();
+                    if (rb != null)
+                        rb.AddExplosionForce(-20, explosionPosition, 20, 3.0F);
 
-                if (rb != null)
-                    rb.AddExplosionForce(20, explosionPosition, 20, 3.0F);
-
+                }
             }
         }
     }
 }
+
